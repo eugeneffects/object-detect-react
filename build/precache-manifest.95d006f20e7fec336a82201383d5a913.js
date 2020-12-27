be14452dd2f4120069fd800fa50bc1cbad439b28
eugeneffects <eugeneffects@gmail.com>
2020-12-27 19:46:27 +0900

    설명글

---
+++ build/precache-manifest.95d006f20e7fec336a82201383d5a913.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a7f621a078ee1445906",
+    "url": "/static/css/main.7e306caf.chunk.css"
+  },
+  {
+    "revision": "5a7f621a078ee1445906",
+    "url": "/static/js/main.5a7f621a.chunk.js"
+  },
+  {
+    "revision": "0cb9d41bebb2373d2d4a",
+    "url": "/static/js/1.0cb9d41b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a2769398380400b8275fe202f122a00",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -97,7 +97,46 @@ class App extends React.Component {
           width="600"
           height="500"
         />
-      </div>
+
+<div className="intro">
+      <br/>
+      <h1>어떻게 구현되나요?</h1>
+      <p>
+        <a href="https://js.tensorflow.org" target="_blank" rel="noopener noreferrer">
+        Tensorflow.js</a>, 로 웹캠에서 물체를 파악하는 딥러닝 모델을 사용합니다.
+        <br/>웹캠을 통한 어떤 정보도 획득하지 않으며, 
+        <br/>모든 프로세스는 당신의 컴퓨터 내에서만 이뤄집니다!
+        <br/>사전 학습된 물체에 대해서만 판별 가능합니다.
+        (서버살 돈이 없어요...)
+      </p>
+      <br/>
+      <h1>제 프로젝트에서도 이런 기술을 사용할 수 있나요?</h1>
+      <p>
+        물론이죠! 아래 링크에서 확인해보세요.
+        <br/>
+        <a href="https://modeldepot.io/mikeshi/tiny-yolo-in-javascript" target="_blank" rel="noopener noreferrer">
+          ModelDepot!
+        </a>
+      </p>
+      <br/>
+      <h1>이거 왜 이렇게 느립니까!?</h1>
+      <p>
+        실시간 물체탐지 기술은 꽤 어려운 일이고, 
+        <br/>대부분의 모델은 강력한 GPU를 갖춘 컴퓨터에서 사용되도록 최적화 되어있습니다.
+        <br/>아마도 GPU를 가지고 있지 않으실 것이라서,(휴대폰이라면 더욱)
+        <br/>최대의 성능을 내긴 어려울 것 입니다.
+      </p>
+      <br/>
+      <h1>작동 원리는 어떻게 되나요?</h1>
+      <p>
+        최신 이미지 인식 기술인
+        <a href="https://pjreddie.com/darknet/yolo/" target="_blank" rel="noopener noreferrer">
+        Tiny YOLO</a> 모델을 사용하고 있으며 
+        <br/>웹에서 사용가능하도록 Tensorflow.js 로 변환되었습니다.
+        <br/>자세한 내용은 링크에서 확인하세요.
+      </p>
+    </div>
+  </div>
     );
   }
 }
